218772518d600c25ab30f1e03ffc562fd839415e
hebrerillo <hebrerillo@gmail.com>
2022-10-17 17:32:31 +0200

    Refactoring the client code into a class

--- client.cpp
+++ client.cpp
@@ -1,144 +1,220 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include "client.h"
 #include "common.h"
-#define DEFAULT_TIME_OUT_SECONDS 4
+#define DEFAULT_TIME_OUT_MSECONDS 4000000
 
-static int socketDescriptor;
-static int pfd[2];
+const char* Client::DEFAULT_IP = "127.0.0.1";
+const int Client::DEFAULT_PORT = 8080;
+const std::chrono::milliseconds Client::DEFAULT_DELAY = std::chrono::milliseconds(1000);
+const std::chrono::milliseconds Client::MAXIMUM_WAITING_TIME_FOR_FLAG = std::chrono::milliseconds(2000);
 
-void socketClient(const char *serverIP = DEFAULT_SERVER_ADDRESS, const char* serverDelay = "1000")
+Client::Client()
+: isRunning_(false)
 {
     int errorNumber;
-    char message[BUFFER_SIZE];
-    struct sockaddr_in serverAddress;
-    memset(message, 0, sizeof(message));
-    strcpy(message, serverDelay);
-
-    socketDescriptor = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-    if (socketDescriptor == -1)
+    if (pipe2(pipeDescriptors_, O_NONBLOCK) == -1)
     {
         errorNumber = errno;
+        std::cerr << "Could not create the pipe file descriptors. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
+    }
+}
+
+bool Client::createSocket()
+{
+    socketDescriptor_ = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    if (socketDescriptor_ == -1)
+    {
+        int errorNumber = errno;
         std::cerr << "Could not create the socket server. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void Client::stop()
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    if (!isRunning_.load())
+    {
         return;
     }
 
+    write(pipeDescriptors_[1], "0", 1);
+    auto quitPredicate = [this]()
+    {
+        return !isRunning_.load();
+    };
+
+    if (!quitCV_.wait_for(lock, MAXIMUM_WAITING_TIME_FOR_FLAG, quitPredicate))
+    {
+        std::cerr << "Time out expired when waiting for pending connections to finish!!!" << std::endl;
+    }
+}
+
+void Client::closeSocketAndNotify()
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    close(socketDescriptor_);
+    isRunning_.exchange(false);
+    quitCV_.notify_one();
+}
+
+bool Client::connectToServer(const char* serverIP, int port)
+{
+    struct sockaddr_in serverAddress;
     serverAddress.sin_addr.s_addr = inet_addr(serverIP);
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_port = htons(port);
+    int errorNumber;
 
-    if (connect(socketDescriptor, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1)
+    if (connect(socketDescriptor_, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1)
     {
         errorNumber = errno;
         if (errorNumber != EINPROGRESS)
         {
             std::cerr << "Could not connect to the server. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
-            return;
+            return false;
         }
     }
 
-    struct timeval timeOut;
-    timeOut.tv_sec = DEFAULT_TIME_OUT_SECONDS;
-    timeOut.tv_usec = 0;
+    return true;
+}
 
+bool Client::consumePipe() const
+{
+    bool done = false;
+    while (!done)
+    {
+        char ch;
+        if (read(pipeDescriptors_[0], &ch, 1) == -1)
+        {
+            int errorNumber = errno;
+            if (errorNumber != EAGAIN)
+            {
+                std::cerr << "Error reading all bytes from the pipe. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
+                return false;
+            }
+            done = true;
+        }
+    }
+
+    return true;
+}
+
+bool Client::sendDelayToServer(const std::chrono::milliseconds& serverDelay, const char* serverIP, int port)
+{
+    char message[BUFFER_SIZE];
     fd_set writeFds;
     fd_set readFds;
+    struct timeval timeOut;
+    timeOut.tv_sec = 0;
+    timeOut.tv_usec = DEFAULT_TIME_OUT_MSECONDS;
+
+    isRunning_.exchange(true);
+
+    if (!createSocket() || !connectToServer(serverIP, port))
+    {
+        closeSocketAndNotify();//TODO check if this is a problem.
+        return false;
+    }
+
+    consumePipe();//To consume data from the read side of the pipe produced on previous calls to this method.
+
     FD_ZERO(&writeFds);
     FD_ZERO(&readFds);
-    FD_SET(socketDescriptor, &writeFds);
-    FD_SET(socketDescriptor, &readFds);
-    FD_SET(pfd[0], &readFds);
+    FD_SET(socketDescriptor_, &writeFds);
+    FD_SET(socketDescriptor_, &readFds);
+    FD_SET(pipeDescriptors_[0], &readFds);
 
-    int retValue = select((pfd[0] > socketDescriptor ? pfd[0] : socketDescriptor) + 1, &readFds, &writeFds, NULL, &timeOut);
+    int retValue = select((pipeDescriptors_[0] > socketDescriptor_ ? pipeDescriptors_[0] : socketDescriptor_) + 1, &readFds, &writeFds, NULL, &timeOut);
 
     if (retValue == -1)
     {
-        errorNumber = errno;
+        int errorNumber = errno;
         std::cerr << "select failed when trying to connect to the server. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
-        return;
+        closeSocketAndNotify();
+        return false;
     }
 
     if (retValue == 0)
     {
         std::cerr << "Time out expired when waiting to connect to the server." << std::endl;
-        close(socketDescriptor);
-        return;
+        closeSocketAndNotify();
+        return false;
     }
 
-    if (FD_ISSET(socketDescriptor, &writeFds))
+    if (FD_ISSET(socketDescriptor_, &writeFds))
     {
-        if (!writeMessage(socketDescriptor, message))
+        memset(message, 0, sizeof(message));
+        strcpy(message, std::to_string(serverDelay.count()).c_str());
+        if (!writeMessage(socketDescriptor_, message))
         {
-            return;
+            closeSocketAndNotify();
+            return false;
         }
     }
 
     FD_ZERO(&readFds);
-    FD_SET(socketDescriptor, &readFds);
-    FD_SET(pfd[0], &readFds);
+    FD_SET(socketDescriptor_, &readFds);
+    FD_SET(pipeDescriptors_[0], &readFds);
 
-    retValue = select((pfd[0] > socketDescriptor ? pfd[0] : socketDescriptor) + 1, &readFds, NULL, NULL, &timeOut);
+    retValue = select((pipeDescriptors_[0] > socketDescriptor_ ? pipeDescriptors_[0] : socketDescriptor_) + 1, &readFds, NULL, NULL, &timeOut);
     if (retValue == -1)
     {
-        errorNumber = errno;
+        int errorNumber = errno;
         std::cerr << "select failed when trying to read from the server. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
-        return;
+        closeSocketAndNotify();
+        return false;
     }
 
     if (retValue == 0)
     {
         std::cerr << "Time out expired when waiting for the server to write." << std::endl;
-        close(socketDescriptor);
-        return;
+        closeSocketAndNotify();
+        return false;
     }
 
-    if (FD_ISSET(pfd[0], &readFds)) //Another thread wrote to the 'write' end of the pipe.
+    if (FD_ISSET(pipeDescriptors_[0], &readFds) && !consumePipe()) //Another thread wrote to the 'write' end of the pipe.
     {
-        for (;;)
-        {
-            char ch;
-            if (read(pfd[0], &ch, 1) == -1)
-            {
-                errorNumber = errno;
-                if (errorNumber == EAGAIN)
-                {
-                    break; /* No more bytes */
-                }
-                else
-                {
-                    std::cerr << "Error reading all bytes from the pipe. Error code " << errorNumber << ": " << strerror(errorNumber) << std::endl;
-                    return;
-                }
-            }
-        }
+        std::cout << "Closing by self pipe trick" << std::endl;
+        closeSocketAndNotify();
+        return false;
     }
-    else if (FD_ISSET(socketDescriptor, &readFds))
+    else if (FD_ISSET(socketDescriptor_, &readFds))
     {
         memset(message, 0, sizeof(message));
-        if (!readMessage(socketDescriptor, message))
+        if (!readMessage(socketDescriptor_, message))
         {
-            return;
+            closeSocketAndNotify();
+            return false;
         }
-        printf("Recibo del servidor = %s\n", message);
     }
 
-    close(socketDescriptor);
+    closeSocketAndNotify();
+    return true;
+}
+
+Client::~Client()
+{
+    close(pipeDescriptors_[0]);
+    close(pipeDescriptors_[1]);
 }
 
 int main(int argc, char* argv[])
 {
-    if (pipe2(pfd, O_NONBLOCK) == -1)
-    {
-        std::cerr << "select failed when trying to read from the server. Error code " << errno << ": " << strerror(errno) << std::endl;
-        return 0;
-    }
+    Client client;
 
-    std::thread threadClient(socketClient, "127.0.0.1", argv[1]);
-    //std::this_thread::sleep_for(std::chrono::milliseconds(1200));
-    //write(pfd[1], "0", 1);
+    std::thread threadClient([&client]()
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5200));
+        client.stop();
+    });
+
+    client.sendDelayToServer(std::chrono::milliseconds(atoi(argv[1])));
+
     threadClient.join();
 
-    close(pfd[0]);
-    close(pfd[1]);
-    
     return 0;
 }
---
+++ client.h
@@ -0,0 +1,74 @@
+#ifndef PT_CLIENT_H
+#define PT_CLIENT_H
+
+#include <chrono>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+
+class Client
+{
+public:
+    static const char* DEFAULT_IP;
+    static const int DEFAULT_PORT;
+    static const std::chrono::milliseconds DEFAULT_DELAY;
+    static const std::chrono::milliseconds MAXIMUM_WAITING_TIME_FOR_FLAG; //The maximum waiting time for the flag 'isRunning_' to be cleared.
+
+    /**
+     * Constructor.
+     * Creates the pipe file descriptors for 'pipeDescriptors_'.
+     */
+    Client();
+
+    bool sendDelayToServer(const std::chrono::milliseconds& serverDelay = DEFAULT_DELAY, const char* serverIP = DEFAULT_IP, int port = DEFAULT_PORT);
+
+    /**
+     * Quits any pending connection by a previous call to 'sendDelayToServer' by using the self pipe trick.
+     * This call blocks waiting until a maximum time of MAXIMUM_WAITING_TIME_FOR_FLAG for the flag 'isRunning_' to be cleared.
+     */
+    void stop();
+
+    /**
+     * Destructor.
+     * Closes the pipe file descriptors in 'pipeDescriptors_'.
+     */
+    ~Client();
+
+private:
+
+    /**
+     * Creates a non-blocking socket on the file descriptor 'socketDescriptor_'.
+     *
+     * @return true if the socket was created successfully, false otherwise.
+     */
+    bool createSocket();
+
+    /**
+     * Performs a connection operation on 'socketDescriptor_'.
+     *
+     * @param[in] serverIP The IP address of the remote server.
+     * @param[in] port The port where the remote server is listening to connections.
+     * @return true if the connection operation was succesfull, false otherwise.
+     */
+    bool connectToServer(const char* serverIP = DEFAULT_IP, int port = DEFAULT_PORT);
+
+    /**
+     * Consumes all the pending data in the read end pipe 'pipeDescriptors_[0]'.
+     *
+     * @return true if the reading operation is successful, false otherwise.
+     */
+    bool consumePipe() const;
+    
+    /**
+     * Close the socket descriptor 'socketDescriptor_' and clears the flag 'isRunning_' to notify on threads waiting on 'quitCV_'.
+     */
+    void closeSocketAndNotify();
+
+    int socketDescriptor_;
+    int pipeDescriptors_[2]; //The file descriptors involved in the 'Self pipe trick'
+    std::mutex mutex_;
+    std::atomic<bool> isRunning_; //True if there is a pending connection, false otherwise.
+    std::condition_variable quitCV_; //To notify to the main that there are no pending connections.
+};
+
+#endif
